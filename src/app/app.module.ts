c5c7a9e801152cf91ff4a7e0c7b8cb1f6246f1bb
siva20456 <siva2002ismart2002@gmail.com>
2023-12-02 15:11:43 +0530

    In Angular Project Added Landing Page

---
+++ src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { LandingComponentComponent } from './landing-component/landing-component.component';
+import { HomeComponentComponent } from './home-component/home-component.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LandingComponentComponent,
+    HomeComponentComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    CommonModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
